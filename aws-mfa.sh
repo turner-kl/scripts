4d2e415a0f0a37b55805a60ec2b1169e26aa7c3d
Ryo Tanaka <ryo2tanaka@tohmatsu.co.jp>
2022-02-07 11:43:06 +0900

    modify aws mfa

---
+++ aws-mfa.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+mfaAuth() {
+    echo "Please input MFA code"
+    read -r input
+
+    echo "setting $1 profile using mfa ..."
+    mfaDeviceSerial=$(aws configure --profile $cmd get mfa_serial)
+    mfaToken=$(aws sts get-session-token --serial-number "$mfaDeviceSerial" --token-code "$input" --profile $1)
+    aws configure set profile.$1-mfa.aws_access_key_id $(echo "$mfaToken" | jq -r '.Credentials.AccessKeyId')
+    aws configure set profile.$1-mfa.aws_secret_access_key $(echo "$mfaToken" | jq -r '.Credentials.SecretAccessKey')
+    aws configure set profile.$1-mfa.aws_session_token $(echo "$mfaToken" | jq -r '.Credentials.SessionToken')
+    echo "expiration : $(echo "$mfaToken" | jq -r '.Credentials.Expiration')"
+    echo "completed."
+}
+
+echo "Select aws profile"
+
+select cmd in $(aws configure list-profiles); do
+    echo "set $cmd profile"
+    mfaAuth $cmd
+    break
+done
